0e1d9650ab25c013d51a693a731da58da78c4478
Priyesh Wani <priyesh@unity3d.com>
2018-06-25 10:50:18 +0200

    first commit

---
+++ Assets/ECSScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using UnityEditor;
+
+public class ECSScript : MonoBehaviour
+{
+
+	public float speed;
+
+}
+
+class ECSSystem : ComponentSystem
+{
+	struct Components
+	{
+	 	public ECSScript scriptComponent;
+		public Transform componentTransform;
+
+	}
+	protected override void OnUpdate()
+	{
+		float time = Time.deltaTime;
+		foreach (var e in GetEntities<Components>())
+		{
+			e.componentTransform.Rotate(time*e.scriptComponent.speed,time*e.scriptComponent.speed,0f);
+		}
+	}
+}
---
+++ Assets/Enemy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using Unity.Entities;
+
+public class Enemy : MonoBehaviour
+{
+    public Transform playerPosition;
+}
+
+
+class EnemySystem : ComponentSystem
+{
+    struct EnemyComponents
+    {
+        public Enemy enemy;
+        public NavMeshAgent agent;
+    }
+    protected override void OnUpdate()
+    {
+        foreach (var e in GetEntities<EnemyComponents>())
+        {
+            e.agent.SetDestination(e.enemy.playerPosition.position);
+        }
+    }
+}
